6dbc05c058bca6743ea4434c91d9c9d5af84dde9
Satpal Tanan <tanansatpal@gmail.com>
2020-08-11 03:11:42 +0530

    Basic page with data and UI copied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HackernewsService} from './services/hackernews.service';
+import {forkJoin} from 'rxjs';
+
+interface Story {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: string[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HackernewsService]
 })
-export class AppComponent {
-  title = 'hackernan';
+export class AppComponent implements OnInit {
+  stories: Story[];
+
+  constructor(private hnService: HackernewsService) {
+  }
+
+  ngOnInit(): any {
+    this.hnService.getNewStories().subscribe(data => {
+      const observables = data.slice(0, 20).map(item => this.hnService.getItem(item));
+      forkJoin(...observables).subscribe(results => {
+        this.stories = results;
+      });
+    });
+
+  }
 }
